f2bc73b50c20a3020f59c3190b2f18ebb9ddc1fa
Esmin53 <esmin.tufekcic53@gmail.com>
2023-06-15 02:54:32 +0200

    Vjerovatno zadnji commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -21,6 +21,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->spinBox_god->setButtonSymbols(QAbstractSpinBox::NoButtons);
     ui->spinBox_god->setValue(currentYear);
 
+    ui->pushButton_update->setDisabled(true);
+    ui->pushButton_obrisi->setDisabled(true);
+
     QSqlDatabase mydb = QSqlDatabase::addDatabase("QSQLITE");
     mydb.setDatabaseName("C:/Users/MAGARAC/Desktop/ClickChargeET.db");
 
@@ -28,6 +31,7 @@ MainWindow::MainWindow(QWidget *parent) :
         ui->label_db->setText("Failed to open the database!");
     else
         ui->label_db->setText("Conected...");
+
     displayDataInTableView();
 
 }
@@ -39,37 +43,38 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_dodaj_clicked()
 {
-    QString serijskiBroj, inventurniBroj, model, lokacija, osoba, rb, psb, tsb;
-    serijskiBroj=ui->lineEdit_sb->text();
-    inventurniBroj=ui->lineEdit_ib->text();
-    model=ui->lineEdit_mp->text();
-    lokacija=ui->lineEdit_l->text();
-    osoba=ui->lineEdit_zo->text();
-    rb=ui->lineEdit_rb->text();
-    psb=ui->lineEdit_psb->text();
-    tsb=ui->lineEdit_tsb->text();
+    QString serijskiBroj, inventurniBroj, model, lokacija, osoba;
+    int rb, psb, tsb;
 
+    serijskiBroj = ui->lineEdit_sb->text();
+    inventurniBroj = ui->lineEdit_ib->text();
+    model = ui->lineEdit_mp->text();
+    lokacija = ui->lineEdit_l->text();
+    osoba = ui->lineEdit_zo->text();
+    rb = ui->lineEdit_rb->text().toInt();
+    psb = ui->lineEdit_psb->text().toInt();
+    tsb = ui->lineEdit_tsb->text().toInt();
 
     QSqlQuery qry;
 
     QString query = "insert into Printeri (redniBroj, serijskiBroj, inventurniBroj, model, lokacija, osoba, prethodnoStanjeBrojaca, trenutnoStanjeBrojaca)"
-                    "values ('"+rb+"', '"+serijskiBroj+"', '"+inventurniBroj+"', '"+model+"', '"+lokacija+"', '"+osoba+"','"+psb+"', '"+tsb+"');";
+                    "values ('" + QString::number(rb) + "', '" + serijskiBroj + "', '" + inventurniBroj + "', '" + model + "', '" + lokacija + "', '" + osoba + "', '" + QString::number(psb) + "', '" + QString::number(tsb) + "');";
 
-    QString query_2 = "CREATE TABLE '"+serijskiBroj+"' ("
-                                                    "godina INT,"
-                                                    "mjesec INT,"
-                                                    "prethodno INT,"
-                                                    "trenutno INT,"
-                                                    "potrosnja INT,"
-                                                    "cijena REAL"
-                                                    ")";
+    QString query_2 = "CREATE TABLE '" + serijskiBroj + "' ("
+                                                      "godina INT,"
+                                                      "mjesec INT,"
+                                                      "prethodno INT,"
+                                                      "trenutno INT,"
+                                                      "potrosnja INT,"
+                                                      "cijena REAL"
+                                                      ")";
 
     int godina = ui->spinBox_god->value();
     int mjesec = ui->spinBox_mj->value();
-    int prethodno = psb.toInt();
-    int trenutno = tsb.toInt();
-    int potrosnja = (trenutno - prethodno);
-    int cijena = (potrosnja * 0.019);
+    int prethodno = psb;
+    int trenutno = tsb;
+    int potrosnja = trenutno - prethodno;
+    float cijena = potrosnja * 0.019;
 
     QString query_3 = QString("INSERT INTO %1 (godina, mjesec, prethodno, trenutno, potrosnja, cijena) "
                               "VALUES (%2, %3, %4, %5, %6, %7)")
@@ -81,20 +86,18 @@ void MainWindow::on_pushButton_dodaj_clicked()
                       .arg(potrosnja)
                       .arg(cijena);
 
-    if(qry.exec(query)) {
-        QMessageBox::information(this, tr("Save"), tr("Saved"));
-            if(qry.exec(query_2)) {
-                QMessageBox::information(this, tr("Save"), tr("Saved"));
-                    if(qry.exec(query_3)) {
-                        QMessageBox::information(this, tr("Save"), tr("Saved"));
-                    } else {
-                        QMessageBox::critical(this, tr("error::"), qry.lastError().text());
-                    }
-             } else {
-                QMessageBox::critical(this, tr("error::"), qry.lastError().text());
-             }
+    if (qry.exec(query)) {
+        if (qry.exec(query_2)) {
+            if (qry.exec(query_3)) {
+                QMessageBox::information(this, tr("Uspjeh"), tr("Uspješno ste dodali novi printer!"));
+            } else {
+                QMessageBox::critical(this, tr("Error"), qry.lastError().text());
+            }
+        } else {
+            QMessageBox::critical(this, tr("Error"), qry.lastError().text());
+        }
     } else {
-        QMessageBox::critical(this, tr("error::"), qry.lastError().text());
+        QMessageBox::critical(this, tr("Error"), qry.lastError().text());
     }
 }
 
@@ -118,13 +121,13 @@ void MainWindow::on_pushButton_update_clicked()
     int potrosnja = (trenutno - prethodno);
     int cijena = (potrosnja * 0.019);
 
-        // Check if a row with the same month and year already exists
+        // Provjera
         QSqlQuery query;
         query.prepare("SELECT * FROM "+serijskiBroj+" WHERE mjesec = :mjesec AND godina = :godina");
         query.bindValue(":mjesec", mjesec);
         query.bindValue(":godina", godina);
         if (query.exec() && query.next()) {
-            // Row with the same month and year already exists, update the row
+            // Apdejtaj red ako ima zapis za ovaj mjesec
             query.prepare("UPDATE "+serijskiBroj+" SET prethodno = :prethodno, trenutno = :trenutno, potrosnja = :potrosnja, cijena = :cijena WHERE mjesec = :mjesec AND godina = :godina");
             query.bindValue(":prethodno", prethodno);
             query.bindValue(":trenutno", trenutno);
@@ -134,13 +137,12 @@ void MainWindow::on_pushButton_update_clicked()
             query.bindValue(":godina", godina);
             if (query.exec()) {
                 qDebug() << "Row updated successfully";
-                // Perform any additional actions after updating the row
+                 QMessageBox::information(this, tr("Uspješno"), tr("Dodali ste novi red"));
             } else {
                 qDebug() << "Failed to update row:" << query.lastError().text();
-                // Handle the error
             }
         } else {
-            // Row with the same month and year doesn't exist, add a new row
+            // Dodaj novi red ako nema zapisa za ovaj mjesec
             query.prepare("INSERT INTO "+serijskiBroj+" (godina, mjesec, prethodno, trenutno, potrosnja, cijena) "
                           "VALUES (:godina, :mjesec, :prethodno, :trenutno, :potrosnja, :cijena)");
             query.bindValue(":godina", godina);
@@ -151,32 +153,53 @@ void MainWindow::on_pushButton_update_clicked()
             query.bindValue(":cijena", cijena);
             if (query.exec()) {
                 qDebug() << "New row added successfully";
-                // Perform any additional actions after adding the new row
+                QMessageBox::information(this, tr("Uspješno"), tr("Dodali ste novi red"));
             } else {
                 qDebug() << "Failed to add new row:" << query.lastError().text();
-                // Handle the error
             }
         }
+
+        ui->label_sb->setText("");
+        ui->lineEdit_rb->setText("");
+        ui->lineEdit_sb->setText("");
+        ui->lineEdit_ib->setText("");
+        ui->lineEdit_mp->setText("");
+        ui->lineEdit_l->setText("");
+        ui->lineEdit_zo->setText("");
+        ui->lineEdit_psb->setText("");
+        ui->lineEdit_tsb->setText("");
+        ui->pushButton_update->setDisabled(true);
+        ui->pushButton_obrisi->setDisabled(true);
+        ui->pushButton_dodaj->setDisabled(false);
 }
 
 void MainWindow::on_pushButton_obrisi_clicked()
 {
-    // Get the value needed to identify the row to delete
     QString serijskiBroj = ui->lineEdit_sb->text();
 
-    // Prepare the delete query
     QSqlQuery query;
     query.prepare("DELETE FROM Printeri WHERE serijskiBroj = :serijskiBroj");
     query.bindValue(":serijskiBroj", serijskiBroj);
 
-    // Execute the delete query
     if (query.exec()) {
         qDebug() << "Row deleted successfully";
-        // Perform any additional actions after deleting the row
+                QMessageBox::information(this, tr("Uspješno"), tr("Izbrisali ste podatke o printeru"));
     } else {
         qDebug() << "Failed to delete row:" << query.lastError().text();
-        // Handle the error
     }
+
+    ui->label_sb->setText("");
+    ui->lineEdit_rb->setText("");
+    ui->lineEdit_sb->setText("");
+    ui->lineEdit_ib->setText("");
+    ui->lineEdit_mp->setText("");
+    ui->lineEdit_l->setText("");
+    ui->lineEdit_zo->setText("");
+    ui->lineEdit_psb->setText("");
+    ui->lineEdit_tsb->setText("");
+    ui->pushButton_update->setDisabled(true);
+    ui->pushButton_obrisi->setDisabled(true);
+    ui->pushButton_dodaj->setDisabled(false);
 }
 
 void MainWindow::on_pushButton_m_clicked()
@@ -203,6 +226,10 @@ void MainWindow::displayDataInTableView()
 
 void MainWindow::on_tableView_activated(const QModelIndex &index)
 {
+    ui->pushButton_update->setDisabled(false);
+    ui->pushButton_obrisi->setDisabled(false);
+    ui->pushButton_dodaj->setDisabled(true);
+
     QString val = ui->tableView->model()->data(index).toString();
         int columnIndex = index.column();
         QSqlQuery query;
@@ -214,11 +241,9 @@ void MainWindow::on_tableView_activated(const QModelIndex &index)
             query.prepare("SELECT redniBroj, serijskiBroj, inventurniBroj, model, lokacija, osoba, prethodnoStanjeBrojaca, trenutnoStanjeBrojaca FROM Printeri WHERE serijskiBroj = :val");
             query.bindValue(":val", val);
 
-            // Execute the query
             if (query.exec()) {
-                // Check if there is a row of data
                 if (query.next()) {
-                    // Fetch the data from the current row and assign it to the variables
+                    // Fetch data iz serijskog broja
                     serijskiBroj = query.value("serijskiBroj").toString();
                     inventurniBroj = query.value("inventurniBroj").toString();
                     model = query.value("model").toString();
@@ -238,7 +263,7 @@ void MainWindow::on_tableView_activated(const QModelIndex &index)
                     ui->lineEdit_psb->setText(psb);
                     ui->lineEdit_tsb->setText(tsb);
 
-                    //Dio koda za prikaz podataka u drugi tableView
+                    //Dio koda za prikaz podataka u drugu tableView
                     QSqlQueryModel *model = new QSqlQueryModel(this);
                     model->setQuery("SELECT * FROM " + val);
 
@@ -248,11 +273,11 @@ void MainWindow::on_tableView_activated(const QModelIndex &index)
 
 
                 } else {
-                    // Handle the case when no rows are returned
+                    // Ako ne nađe podatke
                     qDebug() << "No data found";
                 }
             } else {
-                // Handle the case when the query execution fails
+                // Ako neki error bude
                 qDebug() << "Failed to execute query:" << query.lastError().text();
             }
         }
